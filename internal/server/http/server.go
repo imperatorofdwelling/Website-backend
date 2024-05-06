c00cdf5ce16df79d2e8b6f1d05d07e0d166b7041
Rasikrr <rs.t.95@mail.ru>
2024-05-06 15:32:35 +0500

    server and router(chi) were created

--- internal/server/http/server.go
+++ internal/server/http/server.go
@@ -1,1 +1,34 @@
 package http
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	srv *http.Server
+}
+
+// TODO create configs and create server from cfg
+
+func New() *Server {
+	srv := &http.Server{
+		Addr:         "localhost:8080",
+		ReadTimeout:  time.Second * 4,
+		WriteTimeout: time.Second * 4,
+		IdleTimeout:  time.Second * 60,
+		Handler:      newRouter(),
+	}
+	return &Server{
+		srv: srv,
+	}
+}
+
+// Creating chi router
+func newRouter() http.Handler {
+	r := chi.NewRouter()
+	// There we need to write endpoints and middlewares
+
+	return r
+}
