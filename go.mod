fb94cf6a60c42e51c69c13e992b41477a76bf631
AnsiLucky <15asktt@gmail.com>
2024-05-07 16:24:19 +0500

    repository skeleton

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/https-whoyan/dwellingPayload
 
 go 1.21.1
+
+require github.com/jmoiron/sqlx v1.4.0 // indirect
---
+++ pkg/repository/db.go
@@ -0,0 +1,21 @@
+package repository
+
+import (
+	"github.com/jmoiron/sqlx"
+)
+
+type Authorization interface {
+	//CreateUser(user User) (int, error)
+	//GetUser(username, password string) (todo.User, error)
+}
+
+type Repository struct {
+	Authorization
+	// TODO: transaction ...
+}
+
+func NewRepository(db *sqlx.DB) *Repository {
+	return &Repository{
+		Authorization: NewAuthPostgres(db),
+	}
+}
